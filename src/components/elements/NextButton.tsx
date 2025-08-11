43a66f190b2068a829574b31d8be8e3cd58c44cf
Hoang Nam <nam23062002@gmail.com>
2025-08-12 01:01:51 +0700

    refactor: Replace useGlobal

--- src/components/elements/NextButton.tsx
+++ src/components/elements/NextButton.tsx
@@ -1,41 +1,25 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { useGlobalStore } from "@/lib/store/useGlobalStore";
-
+import Link from "next/link";
+import { useLoading } from "@/lib/hooks/useStores";
 
 export const NextButton = ({ content, class: className, href }: { content: string; class: string; href: string }) => {
-  const setLoading = useGlobalStore((s) => s.setLoading)
+  const { setLoading } = useLoading();
+
+  const handleClick = () => {
+    setLoading(true, `Đang chuyển đến ${content.toLowerCase()}...`);
+  };
 
   return (
-    <Link href={href}
-          onClick={() => setLoading(true)}
-          className={`relative inline-flex h-12 active:scale-95 overflow-hidden rounded-lg p-[1px] focus:outline-none ${className}`}
-          
-        >
-          <span
-            className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"
-          >
-          </span>
-          <span
-            className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 undefined"
-          >
-            {content}
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 448 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M429.6 92.1c4.9-11.9 2.1-25.6-7-34.7s-22.8-11.9-34.7-7l-352 144c-14.2 5.8-22.2 20.8-19.3 35.8s16.1 25.8 31.4 25.8H224V432c0 15.3 10.8 28.4 25.8 31.4s30-5.1 35.8-19.3l144-352z"
-              ></path>
-            </svg>
-          </span>
-     
-        </Link>
-  )
-}
+    <Link href={href} onClick={handleClick}>
+      <button
+        className={`group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-neutral-950 px-6 font-medium text-neutral-200 transition hover:scale-110 ${className}`}
+      >
+        <span>{content}</span>
+        <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-13deg)_translateX(100%)]">
+          <div className="relative h-full w-8 bg-white/20"></div>
+        </div>
+      </button>
+    </Link>
+  );
+};
