d1db1ddc0148f7bceb839bc06663f4b2a6d62eec
Hoang Nam <nam23062002@gmail.com>
2026-01-28 17:02:29 +0700

    feat: Add a utility function to generate Sanity image URLs.

--- src/sanity/lib/sanityImageUrl.ts
+++ src/sanity/lib/sanityImageUrl.ts
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // sanityImageUrl.ts
-import createImageUrlBuilder from '@sanity/image-url'
+import createImageUrlBuilder from "@sanity/image-url";
 
-import { client } from './client' // see example client config
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { client } from "./client"; // see example client config
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 // Create an image URL builder using the client
-const builder = createImageUrlBuilder(client)
+const builder = createImageUrlBuilder(client as any);
 
 // Export a function that can be used to get image URLs
 export function urlFor(source: SanityImageSource) {
-  return builder.image(source)
+  return builder.image(source);
 }
