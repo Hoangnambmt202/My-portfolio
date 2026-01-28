50d2314c62a6f716e9e4d9d54ca459411f8d3a9c
Hoang Nam <nam23062002@gmail.com>
2026-01-28 15:41:34 +0700

    feat: add root layout with global styles, fonts, metadata, and Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import BackToTopButton from "@/components/common/BackToTopButton";
-import { SanityLive } from "@/sanity/lib/live";
-import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter, Poppins, Roboto } from "next/font/google";
+import BackToTopButton from "@/components/common/BackToTopButton";
+import { SanityLive } from "@/sanity/lib/live";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "@/styles/globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,16 +30,20 @@ export const metadata: Metadata = {
   description: "Personal portfolio website showcasing my projects and skills",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
-    
       className={`${inter.variable} ${poppins.variable} ${roboto.variable}`}
     >
       <body className="bg-[#202020] min-h-screen">
         {children}
-      <BackToTopButton/>
-      <SanityLive />
+        <BackToTopButton />
+        <SanityLive />
+        <SpeedInsights />
       </body>
     </html>
   );
