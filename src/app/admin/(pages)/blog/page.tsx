e1086f315461e4c260e28a91800c752c04a894ca
Hoang Nam <nam23062002@gmail.com>
2026-02-25 00:52:21 +0700

    feat: introduce admin blog management dashboard with post listing and analytics.

--- src/app/admin/(pages)/blog/page.tsx
+++ src/app/admin/(pages)/blog/page.tsx
@@ -1,7 +1,7 @@
 // app/(admin)/blog/BlogManager.tsx
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
-import BlogManagerClient from "@/components/admin/BlogManagerClient";
+import BlogManagerClient from "@/components/admin/blog/BlogManagerClient";
 
 export default async function BlogManager() {
   const { data: posts } = await sanityFetch({
