520a0ef9689002bba3e964fc6412a45189572f05
Hoang Nam <nam23062002@gmail.com>
2026-03-06 14:51:19 +0700

    feat: Implement admin settings page with dedicated tabs for general, profile, SEO, integrations, and appearance configurations.

--- src/app/admin/(pages)/settings/tabs/AppearanceTab.tsx
+++ src/app/admin/(pages)/settings/tabs/AppearanceTab.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import Toggle  from "@/components/ui/Toggle";
--- src/app/admin/(pages)/settings/tabs/SeoTab.tsx
+++ src/app/admin/(pages)/settings/tabs/SeoTab.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, KeyboardEvent } from "react";
 import {
   Search,
